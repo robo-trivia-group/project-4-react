e94782855e89565c12ef421e0f27973e8991fe56
Carman Mach <mach.carman@gmail.com>
2021-03-18 15:37:17 -0400

    sorted scores in ascending order and is displaying on page

--- src/HeaderComponent.jsx
+++ src/HeaderComponent.jsx
@@ -3,32 +3,34 @@ import {useEffect, useState} from 'react';
 
 function HeaderComponent() {
 
-  // const [ scoreBoard, setScoreBoard ] = useState([]);
-  
-  // get data from firebase, store into an array
-  // make a copy of sort by either reduce()
-  // set sorted into state
+  const [ scoreBoard, setScoreBoard ] = useState([]);
 
   useEffect(() => {
     const dbScores = firebase.database().ref('/highScore');
     dbScores.on('value', (data) => {
       const dataVal = data.val()
-      console.log(dataVal);
+
+      const unsortedArr = [];
+
+      for (let key in dataVal) {
+        unsortedArr.push(dataVal[key]);
+      }
+
+      const sortedArr = unsortedArr.sort((a, b) => {
+        return  b.highScore - a.highScore
+      })
+
+      const limitedScore = [];
+
+      limitedScore.push(sortedArr[0]);
+      limitedScore.push(sortedArr[1]);
+      limitedScore.push(sortedArr[2]);
+      limitedScore.push(sortedArr[3]);
+      limitedScore.push(sortedArr[4]);
+      
+      setScoreBoard(limitedScore);
     })
-      // console.log(data.val());
-      // const recordedScores = data.val();
-      // const scoreArray = [];
-      // scoreArray.push(recordedScores);
-      // setScoreBoard(scoreArray);
   }, [])
-
-  // const sortedArray = scoreBoard.sort((a, b) => {
-  //   return parseFloat(a.highScore) - parseFloat(b.highScore);
-    
-  // })
-  
-  // console.log(sortedArray);
-
 
   return (
     <header>
@@ -36,8 +38,19 @@ function HeaderComponent() {
         <h1>Robo Trivia</h1>  
       </div>  
 
-      <div>
+      <div className="scoreContainer">
+        <h2>Genius-Bots:</h2>
+        <ul className="highScores">
+          {
+            scoreBoard.map((score, index) => {
+              const {username, highScore} = score;
+              return (
+                <li key={index}>{username}: {highScore} points</li>
+              )
+            })
+          }
 
+        </ul>
       </div>
     </header>
   );
